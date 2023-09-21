06e9a27f7a44e97da57d4706c03b5795bd8ea0bf
Clement <baafik05@gmail.com>
2023-09-21 20:56:47 +0000

    done with start screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromARGB(255, 103, 151, 247),
-                  Color.fromARGB(255, 34, 53, 228),
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-            ),
-            child: StartScreen()),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(context) {
@@ -12,14 +14,26 @@ class StartScreen extends StatelessWidget {
           Image.asset(
             '/Users/gebruiker/Developer/flutter_projects/quiz_app/quiz_app/assets/quiz-logo.png',
             width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
           ),
-          SizedBox(
+          const SizedBox(
             height: 80,
           ),
           const Text(
             'Learn Flutter the fun way!',
             style: TextStyle(
                 color: Color.fromARGB(255, 207, 211, 234), fontSize: 24),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+            onPressed: startQuiz,
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
           ),
         ],
       ),
